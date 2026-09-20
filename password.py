2ecf7f4fb887752f3a6da02dae45e471eed67210
Joseph Juma <jj4443193@gmail.com>
2026-09-20 09:05:53 +0000

    password generator on the way.

---
+++ password.py
@@ -0,0 +1,20 @@
+import random
+import string
+
+length = int(input("password length: "))
+
+chars = (
+    string.ascii_letters +
+    string.digits +
+    string.punctuation
+    
+)
+
+password = "" .join(
+    random.choice(chars)
+    for _ in range(length)
+    
+)
+
+print("\nGenerated password:")
+print(password)
